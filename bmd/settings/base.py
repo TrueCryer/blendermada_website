1b61842bd7e91f40b2cf3c6b8f189d5ab31a697f
truecryer <truecryer@yandex.ru>
2015-11-15 15:30:57 +0300

    Fixed settings file

--- bmd/settings/base.py
+++ bmd/settings/base.py
@@ -59,8 +59,10 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 )
 
 WSGI_APPLICATION = 'bmd.wsgi.application'
