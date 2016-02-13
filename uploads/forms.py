e3dcd13a19799d73810a025a195c92bd0c176003
truecryer <truecryer@yandex.ru>
2016-02-13 22:40:49 +0300

    Some changes in templates

--- uploads/forms.py
+++ uploads/forms.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django import forms
+from django.forms import ValidationError
 
 from .models import Upload, ENGINE_VERSIONS, SCENE_TYPES
 
@@ -63,6 +64,17 @@ class UploadForm(forms.Form):
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
         super(UploadForm, self).__init__(*args, **kwargs)
+
+    def clean_file(self):
+        file_field = self.cleaned_data.get('file', False)
+        if file_field:
+            if file_field._size > 10*1024*1024:
+                raise ValidationError(' too large ( > 10MB )')
+            elif file_field.name.split('.')[-1] != 'blend':
+                raise ValidationError(' isn\'t *.blend file')
+            return file_field
+        else:
+            raise ValidationError('Couldn\'t read uploaded file')
 
     def save_data(self):
         instance = Upload(
